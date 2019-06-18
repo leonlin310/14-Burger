bb38cb8b4ae98698f462e2403afdfd8ac10056f5
Leon Lin <leonlin310@gmail.com>
2019-06-17 21:36:55 -0500

    working basic functionality

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (name) VALUES ('Cheeseburger');
 INSERT INTO burgers (name) VALUES ('Hamburger');
--- INSERT INTO burgers (name, sleepy) VALUES ('Sylvester', true);
--- INSERT INTO burgers (name, sleepy) VALUES ('Marilyn Manson', true);
--- INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
+INSERT INTO burgers (name, eaten) VALUES ('Sylvester', true);
+INSERT INTO burgers (name, eaten) VALUES ('Marilyn Manson', true);
+INSERT INTO burgers (name, eaten) VALUES ('Joe Biden', true);
 INSERT INTO burgers (name) VALUES ('Fishburger');
