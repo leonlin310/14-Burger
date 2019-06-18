117dbf5ab0e1cf3fac0cb1311906b4ea708b50f9
Leon Lin <leonlin310@gmail.com>
2019-06-17 19:37:21 -0500

    please work

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,6 @@
--- INSERT INTO buyers (buyer_name) VALUES ('Kevon');
--- INSERT INTO buyers (buyer_name) VALUES ('Jane');
-
--- INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('wolf', 'Cassie', 195, 1);
--- INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('bear', 'Rachel', 745, 1);
--- INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('hawk', 'Tobias', 850, 2);
--- INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('gorilla', 'Marco', 400, 2);
--- INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('tiger', 'Jake', 300, 2);
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+-- INSERT INTO burgers (name, sleepy) VALUES ('Sylvester', true);
+-- INSERT INTO burgers (name, sleepy) VALUES ('Marilyn Manson', true);
+-- INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
+INSERT INTO burgers (name) VALUES ('Fishburger');
